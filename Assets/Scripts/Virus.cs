34c3a83765a5f270e055a8cbe195f2d3b0dcfa36
Jamie Anderson <jamie@cryptnology.dev>
2021-09-21 11:15:58 +1000

    Added Virus script, Frightened and White animations for the Viruses

---
+++ Assets/Scripts/Virus.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    public int scatterModeTimer1 = 7;
+    public int chaseModeTimer1 = 20;
+    public int scatteModeTimer2 = 7;
+    public int chaseModeTimer2 = 20;
+    public int scatteModeTimer3 = 5;
+    public int chaseModeTimer3 = 20;
+    public int scatteModeTimer4 = 5;
+    public int chaseModeTimer4 = 20;
+
+    private int modeChangeIteration = 1;
+    private float modeChangeTimer = 0;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
